aba4495b7019e6215f8f8ef2333754723679e7a6
Joel-Claros <j.claros@alumno.um.edu.ar>
2025-09-15 04:31:57 -0300

    Agregar funcion comenzar en dados

--- core/dice.py
+++ core/dice.py
@@ -12,3 +12,19 @@ class Dados:
             return[self.dado_1,self.dado_2,self.dado_1,self.dado_2]
         else:
             return[self.dado_1, self.dado_2]
+    
+    def comenzar(self):
+        empate = 0
+        while empate == 0:
+            self.dado_1 = random.randint(1,6)
+            resultado_1 = self.dado_1 
+            self.dado_2 = random.randint(1,6)
+            resultado_2 = self.dado_2
+            if resultado_1 == resultado_2:
+                empate = 0
+            else:
+                empate = 1
+        if resultado_1 > resultado_2:
+            print("Empiezan las fichas blancas")
+        else: 
+            print("Empiezan las fichas negras")
