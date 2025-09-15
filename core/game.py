39d7f76a76ed287f00b9b0f413dcd636e35efb4b
Joel-Claros <j.claros@alumno.um.edu.ar>
2025-09-15 04:55:59 -0300

    Función cambio de turno

---
+++ core/game.py
@@ -0,0 +1,13 @@
+from player import Jugador
+class Juego:
+    def __init__(self, __turnos__):
+        self.turnos = __turnos__
+        self.jugador_actual = None
+
+    def cambio_de_turno(self, __jugador_1__, __jugador_2__):
+        if jugador_actual == __jugador_1__:
+            jugador_actual = __jugador_2__
+        else:
+            jugador_actual = __jugador_1__
+    
+
